3e7f9760c5d5c3e3f0d47f22ca360e4f468b860f
Justin Lee <leejwg.3@gmail.com>
2018-09-11 08:40:08 +0900

    variables

---
+++ basicJS/JS_basic_Exercises/2_variables.js
@@ -0,0 +1,28 @@
+// Variables
+
+// * To open the JS console cmd + option + j
+
+// add variable "firstName" and "lastName" // so that they equal your name
+
+// create a variable that holds the answer // of "firstName" + " " + "lastName"
+
+// Evaluate this question. What is a + b?
+var a = 34;
+var b = 21;
+a = 2;
+a + b; // what is the answer here?
+
+// What is c equal to?
+var c;
+
+// More Variables
+
+var Me = "you";
+var Me = "MeMe";
+var me = "im slightly different";
+
+// What is the output of me?
+// what is the output of other Me?
+
+// what happens here?
+var whoMe = Me + me;
