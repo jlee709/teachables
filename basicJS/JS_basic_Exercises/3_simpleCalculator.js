1102f0e12a743e298cf18a180eb198d104e0025c
Justin Lee <leejwg.3@gmail.com>
2018-09-11 08:45:39 +0900

    fixed typo

--- basicJS/JS_basic_Exercises/3_simpleCalculator.js
+++ basicJS/JS_basic_Exercises/3_simpleCalculator.js
@@ -1,20 +1,16 @@
-// use your javascript to make a simple Calculator! 
+// use your javascript to make a simple Calculator!
 
-Javascript Calculator Exercise
+// Javascript Calculator Exercise
 
 // Make a Calculator! using prompt(), and variables, make a program that does the following:
 // 1. Prompts the user for first number.
 // 2. Stores that first number
 // 3. Prompts the user for the second number.
-// 4. stores that number and responds with the SUM by using an alert.  
-
+// 4. stores that number and responds with the SUM by using an alert.
 
 // Stretch => Add more math functions to the calculator
-// Super Stretch => Make a calculator that calculates dog years :) 
-    // Dog years would be 1 human year = 7 dog years
+// Super Stretch => Make a calculator that calculates dog years :)
+// Dog years would be 1 human year = 7 dog years
 
-
-
-
-// * Hint = check the prompt() and the alert() methods in the console 
+// * Hint = check the prompt() and the alert() methods in the console
 // use those to create your console calculator!
