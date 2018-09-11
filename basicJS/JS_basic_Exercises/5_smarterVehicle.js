867c8d5a6e4be32d2c7139c8620a7f5690d3d9e1
Justin Lee <leejwg.3@gmail.com>
2018-09-11 09:14:14 +0900

    better vehicle

---
+++ basicJS/JS_basic_Exercises/5_smarterVehicle.js
@@ -0,0 +1,18 @@
+// Make a keyless car EVEN BETTER!
+// We are improving our car from previous exercise now.
+// Solutions to future exercises will be in the .js file (meaning Javascript file) from now on.
+
+var age = prompt("What is your age?");
+
+//1. Make the above code have a function called checkDriverAge(). Whenever you call this function,
+// you will get prompted for age. Use Function Declaration to create this function.
+// Notice the benefit in having checkDriverAge() instead of copying and pasting the
+//function everytime?
+
+//2. Create another function that does the same thing, called checkDriverAge2()
+// using a Function Expression.
+
+//BONUS: Instead of using the prompt. Now, only use the return keyword
+//and make the checkDriverAge() function accept an argument of age, so that if you enter:
+checkDriverAge(21);
+//it returns "Powering On. Enjoy the ride!"
