27b81c4da5592b318899e9f374294753148ba58c
Justin Lee <leejwg.3@gmail.com>
2018-09-11 10:05:47 +0900

    objects

---
+++ basicJS/JS_basic_Exercises/7_objects.js
@@ -0,0 +1,8 @@
+// Create a person object with the following properties
+
+// Name, age, occupation and bloodtype
+
+// Add to the person object a function that can check the age for drinking age
+
+// Make an Object called Circle that calculates the radius of the circle
+// include the function within the circle object
