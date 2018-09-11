1eb717cfc009b811aacf5ab820eae0aae00538a6
Justin Lee <leejwg.3@gmail.com>
2018-09-11 09:11:25 +0900

    smart vehicle

---
+++ basicJS/JS_basic_Exercises/4_smartVehicle.js
@@ -0,0 +1,20 @@
+// Make a keyless car!
+// This car will only let you drive if you are over 16. Make it do the following:
+
+// using prompt() and alert(), ask a user for their age.
+// IF they say they are below 16, respond with:
+// "Sorry, you are too young to drive this car. Powering off"
+
+// IF they say they are 16, respond with:
+// "Congratulations on your first year of driving. Enjoy the ride!"
+
+// IF they say they are over 16, respond with:
+// "Initiating start-up: Peddal to the metal!"
+
+var age = prompt("What is your age?");
+
+// Stretch goals
+
+// Add an extra check if they have a racing license "
+// IF they do AND they are over 16 respond with:
+// "Race Mode Settings Enabled! Ready to Rumble!"
